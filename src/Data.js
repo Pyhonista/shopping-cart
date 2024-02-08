20a620e1098601671b73312a57bcf2a1bbcbcdae
Pyhonista <84766563+Pyhonista@users.noreply.github.com>
2024-02-08 10:58:26 +0600

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "basic",
+    name: "Computer Fundamentals",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "webintro",
+    name: "HTML Fundamentals",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "styleintro",
+    name: "CSS Fudamentals",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "codeintro",
+    name: "JS & Python Fundamentals",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+];
